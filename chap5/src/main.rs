ee416cf6b514ba278a2b6b453c1796d9be8ec44b
Tenji <patricepetit@hotmail.com>
2020-07-19 21:24:28 +0200

    feature/chap5 [WIP]

---
+++ chap5/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    
+    struct Rectangle {
+        width: u32,
+        height: u32
+    }
+
+
+    impl Rectangle {
+
+        fn description(&self) -> String {
+            return  "".to_string();
+        }
+        fn print_description(&self) {
+            println!("Rectangle: {} x {}", self.width, self.height)
+        }
+        fn area(&self) -> u32 {
+            &self.width * &self.height
+        }
+
+        fn is_square(&self) -> bool {
+            self.width == self.height
+        }
+
+        fn can_hold(&self, other: &Rectangle) -> bool {
+            self.width > other.width && self.height > other.height
+        }
+    }
+
+    let rect = Rectangle{width: 19, height: 5};
+
+    rect.print_description();
+
+    println!("rect is a square, {}", rect.area());
+}
